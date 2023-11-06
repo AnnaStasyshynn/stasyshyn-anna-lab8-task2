5b5a41d741c38cf4b1bdafd5bd78e8edf1d6e3a5
AnnaStasyshynn <149354583+AnnaStasyshynn@users.noreply.github.com>
2023-11-06 11:14:28 +0200

    Create puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,21 @@
+"""
+Puzzle
+https://github.com/AnnaStasyshynn/stasyshyn-anna-lab8-task1.git
+"""
+def validate_board(board: list) -> bool:
+    """
+    >>> board = [\
+ "**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****"\
+]
+    >>> validate_board(board)
+    False
+    """
+    pass
