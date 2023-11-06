ad2297b50579d0749a68fda7acb1c59565026e3d
AnnaStasyshynn <149354583+AnnaStasyshynn@users.noreply.github.com>
2023-11-06 22:27:23 +0200

    Added check_sections()

--- puzzle.py
+++ puzzle.py
@@ -19,3 +19,23 @@ def validate_board(board: list) -> bool:
     False
     """
     pass
+def check_section(section: list) -> bool | None:
+    """
+    Check if a section:
+    - contains numbers from 1 to 9
+    - contains unique numbers
+    >>> check_section(['1', '2', '3'])
+    True
+    >>> check_section(['1', '12'])
+    False
+    >>> check_section(['1', '1'])
+    False
+    """
+    nums = [int(n) for n in section if n.isdigit()]
+    nums_set = set(nums)
+    if len(nums) != len(nums_set):
+        return False
+    for i in nums:
+        if i > 9:
+            return False
+    return True
